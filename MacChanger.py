0c0ed89e5e3e917c1313c721aeea78a957d80f22
Prism <45443411+prism99@users.noreply.github.com>
2019-04-22 13:22:03 -0400

    Create MacChanger.py

---
+++ MacChanger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Usage: python MacChanger.py
+import subprocess
+interface = raw_input("What interface are you using? ") # pick interface found in ifconfig
+new_mac = raw_input("New MAC address, ie '00:11:22:33:44:55' ") # pick desired new mac address to change to
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
